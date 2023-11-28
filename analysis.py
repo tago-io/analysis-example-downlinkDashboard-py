15af27b6ef5f026316af2be97b6628003ada252f
mateuscardosodeveloper <mateus.informatica44@gmail.com>
2023-11-28 18:03:30 -0300

    Update doc string

--- analysis.py
+++ analysis.py
@@ -8,17 +8,9 @@ You can get the dashboard template to use here: http://admin.tago.io/template/5f
 Environment Variables
 In order to use this analysis, you must setup the Environment Variable table.
 
-account_token: Your account token. Check bellow how to get this.
 default_PORT: The default port to be used if not sent by the dashboard.
 device_id: The default device id to be used if not sent by the dashboard (OPTIONAL).
 payload: The default payload to be used if not sent by the dashboard (OPTIONAL).
-
-Steps to generate an account_token:
-1 - Enter the following link: https://admin.tago.io/account/
-2 - Select your Profile.
-3 - Enter Tokens tab.
-4 - Generate a new Token with Expires Never.
-5 - Press the Copy Button and place at the Environment Variables tab of this analysis.
 """
 from tagoio_sdk import Resources, Analysis
 from tagoio_sdk.modules.Utils.sendDownlink import sendDownlink
